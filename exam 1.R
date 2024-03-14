4ee7cfb157de84df2dbccce552782cc1f4872ce7
rafcedano <158507987+rafcedano@users.noreply.github.com>
2024-03-14 19:07:16 -0400

    Add files via upload

---
+++ exam 1.R
@@ -0,0 +1,40 @@
+Exam 1
+
+1. the null hypothesis is that there is no differencein marijuana smoking among 21 yo's and 57 yo's .
+the alternative hypothesis is that there is a difference among them.
+    21     57
+0 3737    6960
+1 1311    936
+
+1311/5048 = .2597
+936/7896 = .1185 
+.2597 - .1185 = .1412
+the standard error = .0076 and .0042
+the t-stat = 15.89
+and the p value is close to 0 = .006
+confidence interval .1412 +- .1415
+since the p value is close to 0 we can reject the null hypothesis and say that there is a significant difference among smokers ages 21 and 57. 
+
+2. the null hypothesis is that there is no difference in marijuana usage in people who went to some college and people who got a degree.
+the alt. hypothesis is that there is a difference in marijuana usage among people who attended some college and people who earned a degree. 
+some coll.   degree
+22747      34993
+3555       3958      
+3555/26302 = .1352
+3958/38951 = .1016
+the standard error = .0031 and .0019
+the t-stat is= 11.68
+the p value is close to 0 
+confidence interval=.0432+-.0431
+the results of this hypothesis test conclude that we should reject the null hypothesis because there is a difference in smoking among people who attended some college and people who got a degree.
+3.
+
+4. I want to compare people who completed HS to people who earned a degree and how many people DONT smoke marijuana.
+the null is that there is no difference in amount of smokers on average between people who only completed hs vs. those who earned a degree.
+the alt is that there is a difference.
+hs      degree
+20004     34993     
+3297      3958      
+20004/23301=.8585
+34993/38951=.8983
+
